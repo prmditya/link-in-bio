f26d81083a5aadd89c82e5bb85f0e1ca6261291e
Thoriq Kusuma <thoriqk2@gmail.com>
2024-01-27 19:07:21 +0700

    fix bug

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const text = "Thoriq Kusuma";
-  const speed = 100; // Speed in milliseconds
   const resetDelay = 3000; // Reset every 5 seconds
+  const typedText = document.getElementById('typed-text');
+  let animationFrameId;
+  let lastTime;
 
-  function type() {
-    let index = 0;
-    const typedText = document.getElementById('typed-text');
-    typedText.innerHTML = ''; // Clear previous content
+  function addNextCharacter(time) {
+    if (!lastTime) {
+      lastTime = time;
+    }
 
-    function addNextCharacter() {
+    if (time - lastTime > 100) { // Menambahkan karakter setiap 100 milidetik
       if (index < text.length) {
         typedText.innerHTML += text.charAt(index);
         index++;
-        setTimeout(addNextCharacter, speed);
       }
+      lastTime = time;
     }
 
-    addNextCharacter();
+    if (index < text.length) {
+      animationFrameId = requestAnimationFrame(addNextCharacter);
+    }
   }
 
-  setInterval(type, resetDelay); // Call type function every 5 seconds
+  function startTyping() {
+    index = 0;
+    typedText.innerHTML = '';
+    lastTime = null;
+    addNextCharacter(performance.now());
+  }
+
+  function type() {
+    startTyping();
+    setTimeout(type, resetDelay);
+  }
+
   type(); // Initial call to start the typing effect
+
+  // Pause animation when tab is inactive
+  document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+      cancelAnimationFrame(animationFrameId);
+    } else {
+      startTyping();
+    }
+  });
 });
+
+
 
 function redirectTo(url) {
   if (url == undefined) { alert("Coming Soon!!"); return; }
